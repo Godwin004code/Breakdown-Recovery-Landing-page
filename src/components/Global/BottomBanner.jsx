6f8a93c7c3ef97d5accaf1680818a6d45d35f821
Godwin004code <godwinatomicsoda@gmail.com>
2024-09-04 15:58:16 +0100

    mobile responsiveness

--- src/components/Global/BottomBanner.jsx
+++ src/components/Global/BottomBanner.jsx
@@ -6,7 +6,7 @@ const BottomBanner = ({img}) => {
         <section className='my-auto'>
             <h2 className='text-white text-5xl sm:text-2xl sm:text-center'>Don’t miss out</h2>
             <p className='text-white text-base sm:text-sm sm:text-center font-normal mt-3 mb-16 sm:mb-10'>Stay connected and never miss out on important updates, exclusive <br /> offers, and the latest features.</p>
-            <div className='bg-[#fff] sm:hidden p-2 w-fit rounded-xl' style={{border: '1px solid #0000000d'}}>
+            <div className='bg-[#fff] z-30 sm:hidden p-2 w-fit rounded-xl' style={{border: '1px solid #0000000d'}}>
 <input type="email" className='h-[49px] pl-4 pr-36 focus:outline-none bg-transparent' placeholder='Your email'  />
 <button className='bg-[#6869ff] text-white my-auto text-sm font-normal h-[49px] px-5 rounded-xl'>Join waitlist</button>
             </div>
@@ -14,7 +14,7 @@ const BottomBanner = ({img}) => {
         <section>
             <img src={img} className='h-[577px] sm:h-[292px]' alt="Banner image" />
         </section>
-        <div className='bg-[#fff] mt-10 p-2 hidden w-full sm:flex justify-between  rounded-xl' style={{border: '1px solid #0000000d'}}>
+        <div className='bg-[#fff] z-40 mt-10 p-2 hidden w-full sm:flex   rounded-xl' style={{border: '1px solid #0000000d'}}>
 <input type="email" className='h-[49px] pl-4 pr-36 sm:pr-0 focus:outline-none bg-transparent' placeholder='Your email'  />
 <button className='bg-[#6869ff] text-white my-auto text-sm font-normal h-[49px] px-5 rounded-xl'>Join waitlist</button>
             </div>
